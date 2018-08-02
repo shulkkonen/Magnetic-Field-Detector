e5b966f690dd80b1aeea65b6b1f0acd6344ca500
Samuli <shulkkonen@luukku.com>
2018-08-02 18:44:30 +0300

    2

--- app/src/main/java/com/shulk/metal/metaldetector/MainActivity.java
+++ app/src/main/java/com/shulk/metal/metaldetector/MainActivity.java
@@ -7,26 +7,31 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ProgressBar;
-import android.widget.TextView;
+import com.github.anastr.speedviewlib.TubeSpeedometer;
+import net.mabboud.android_tone_player.OneTimeBuzzer;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
-    private TextView value;
+
     private SensorManager sensorManager;
     public static DecimalFormat DECIMAL_FORMATTER;
-
     private ProgressBar prg;
+    private TubeSpeedometer tubeSpeedometer;
+    double magnitude = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        value = (TextView) findViewById(R.id.value);
         // define decimal formatter
         DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
         symbols.setDecimalSeparator('.');
         DECIMAL_FORMATTER = new DecimalFormat("#.000", symbols);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        tubeSpeedometer = (TubeSpeedometer) findViewById(R.id.tubeSpeedometer);
 
         prg = (ProgressBar) findViewById(R.id.progressBar);
         //prg.setMax(100);
@@ -50,14 +55,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             float magX = event.values[0];
             float magY = event.values[1];
             float magZ = event.values[2];
-            double magnitude = Math.sqrt((magX * magX) + (magY * magY) + (magZ * magZ));
+            magnitude = Math.sqrt((magX * magX) + (magY * magY) + (magZ * magZ));
             // set value on the screen
             prg.setProgress((int) magnitude);
-            value.setText(DECIMAL_FORMATTER.format(magnitude) + " \u00B5Tesla");
+            tubeSpeedometer.speedTo((int) magnitude);
 
+            playTones();
         }
     }
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    private void playTones(){
+        int sensorValue = (int) magnitude;
+
+        if(sensorValue >= 90){
+
+            OneTimeBuzzer buzzer = new OneTimeBuzzer();
+            buzzer.setDuration(5);
+
+            buzzer.play();
+        }
+
     }
 }
